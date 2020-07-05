94dc6ccbab20a53fbabe23e62601f0eeb8312435
vagrant <vagrant@localhost.localdomain>
2020-07-05 06:03:36 +0000

    [Finish] 3-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,4 +1,17 @@
 class TodolistsController < ApplicationController
   def new
+        # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_paramus)
+    list.save
+    redirect_to '/top'
+  end
+  private
+
+  def list_paramus
+    paramus.require(:list).permit(:title, :body)
   end
 end
